7525f5bb607a32af47dab4780278e2dd5f2a90d2
Łukasz Węgiel <lukasz@mirumee.com>
2016-10-24 12:23:20 +0200

    Add login form

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from . import views
+from django.contrib.auth import views
+from .forms import LoginForm
 
 
 urlpatterns = [
-    url(r'^login/$', views.login_view),
+    url(r'^login/$', views.login, {'template_name': 'account/login.html',
+        'authentication_form': LoginForm}, name='login'),
 ]
--- apps/personal/views.py
+++ apps/personal/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'personal/index.html')
 
 
+@login_required(login_url="/account/login/")
 def contact(request):
     return render(request, 'personal/basic.html', {'content': [
         'Please contact me via this mail:',
